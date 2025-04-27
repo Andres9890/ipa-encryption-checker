22e93a01fa2e0e111ecb8bf295766f483d0f15b4
Andres99 <veryshort8@gmail.com>
2025-04-27 15:14:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -334,4 +334,20 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         return result;
     }
+    
+    const faqItems = document.querySelectorAll('.faq-item');
+    
+    faqItems.forEach(item => {
+        const question = item.querySelector('.faq-question');
+        
+        question.addEventListener('click', () => {
+            item.classList.toggle('active');
+            
+            faqItems.forEach(otherItem => {
+                if (otherItem !== item) {
+                    otherItem.classList.remove('active');
+                }
+            });
+        });
+    });
 });//p
